6437e28170917ff53824843064cf5ab404306476
Dudmatser <atlascmd@gmail.com>
2023-09-17 23:41:08 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess = -1;
+        int total = 1;
+        while (guess != number)
+{
+    Console.Write("Guess a random number: ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (guess < number)
+    {
+    Console.Write("Higher\n");
+    total = total +1;
+    }
+    else if (guess > number)
+    {
+    Console.Write("Lower\n");
+    total = total +1;
+
+    }
+    else 
+    {
+    Console.Write("you got it right!");
+    Console.Write($"You guessed {total} times.");
+    }
+}
+        
     }
 }
