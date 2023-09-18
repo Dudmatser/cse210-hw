14e46e2a809636682c927aa371ebfccdf4769d56
Dudmatser <atlascmd@gmail.com>
2023-09-17 22:50:07 -0600

    finish prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        //string - Strings (a sequence of characters, including letters, numbers, or symbols)
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        //string - Strings (a sequence of characters, including letters, numbers, or symbols)
+        string lname = Console.ReadLine();
+        //use {curl brakets for variables inside of "quotes"} remember to add the $ before the qutoes for this as well.
+        Console.WriteLine($"your name is {lname}, {fname} {lname}");
     }
 }
